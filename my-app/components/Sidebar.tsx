e3da4964ec1b7b889fb2b89de0ccb77f6cafb410
alfeena-01 <alfeenahamza54321@gmail.com>
2026-09-06 21:06:51 +0530

    first commit

--- my-app/components/Sidebar.tsx
+++ my-app/components/Sidebar.tsx
@@ -63,7 +63,7 @@ export default function Sidebar() {
         Fraud <span className="text-white/50">AI</span>
 
     </h1>
-    <p className="text-[10px] uppercase">Security Intelligence </p>
+    <p className="text-[10px] uppercase tracking-[0.2em] text-gray-500">Security Intelligence </p>
 </div>
 
 </div>
