db7220af9229b010d2cb614e27056efa6572f25e
alfeena-01 <alfeenahamza54321@gmail.com>
2026-09-06 21:15:38 +0530

    first commit

--- my-app/components/Sidebar.tsx
+++ my-app/components/Sidebar.tsx
@@ -83,7 +83,7 @@ Monitoring
             <Link
             key={item.name}
             href={item.href}
-            className={`group flex items-center gap-3 rounded-xl px-3 py-3 text-sm  ${
+            className={`group flex items-center gap-3 rounded-xl px-3 py-3 text-sm transition ${
                 active ? "bg-white/10 text-white" : "text-gray-400"
             }`}
             >        
