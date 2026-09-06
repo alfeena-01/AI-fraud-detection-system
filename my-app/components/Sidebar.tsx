ff233bb56ed853bd40bfe846921930079a81e4dc
alfeena-01 <alfeenahamza54321@gmail.com>
2026-09-06 21:28:58 +0530

    first commit

--- my-app/components/Sidebar.tsx
+++ my-app/components/Sidebar.tsx
@@ -110,7 +110,7 @@ Monitoring
 {/*Bottom*/}
 <div className="p-4 border-t border-white/10">
     <div className="mb-3  rounded-xl p-3  hover:bg-white/5"    >
-        <p>System Status</p>
+        <p className="text-xs text-grey-500">System Status</p>
     </div>
 </div>
 
