c8917ef8c42c22d1154506964aa6c185b1f7c298
alfeena-01 <alfeenahamza54321@gmail.com>
2026-09-06 20:44:56 +0530

    first commit

--- my-app/components/Sidebar.tsx
+++ my-app/components/Sidebar.tsx
@@ -2,4 +2,13 @@
 
 import {
     LayoutDashboard,
+    CreditCard,
+    ShieldAlert,
+    BarChart3,
+    BrainCircuit,
+    Settings,
+    LogOut,
+    ShieldCheck,
+} from "lucide-react";
+import { useState } from "react";
 }
