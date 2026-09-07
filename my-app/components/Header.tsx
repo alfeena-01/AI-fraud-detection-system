2ad13565ecf6c42d81e6cca56287fbb5d75930a4
alfeena-01 <alfeenahamza54321@gmail.com>
2026-09-07 20:33:32 +0530

    commit

---
+++ my-app/components/Header.tsx
@@ -0,0 +1,13 @@
+"use client";
+import{Bell, Search } from "lucide-react";
+
+export default function Header() {
+    return (
+        <header className="flex  h-20 items-center justify-between border-b border-white/10 bg-[#080b10]/80 px-8 backdrop-blur-xl">
+
+            <div>
+                <p>
+                    Security/Overview
+                </p>
+            </div>
+        </header>
