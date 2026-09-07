fe0f499c89429abe812f2555e89f85e52dd877a8
alfeena-01 <alfeenahamza54321@gmail.com>
2026-09-07 20:51:41 +0530

    commit

--- my-app/components/Header.tsx
+++ my-app/components/Header.tsx
@@ -32,7 +32,7 @@ export default function Header() {
 <span className="absolute right-2 top-2 h-2 w-2 rounded-full bg-red-500"/>
                 </button>
                 <div className="flex items-center gap-3 border-1 border-white/10 pl-4 ">
-                    <div className="h-9 w-10 rounded-full bg-white text-black">
+                    <div className="flex h-9 w-9 items-center justify-center rounded-full bg-white text-sm font-bold text-black">
 
                     </div>
                 </div>
