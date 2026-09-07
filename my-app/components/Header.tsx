0b96ff2b7800e1d0aade511e5e5530a2e69306b2
alfeena-01 <alfeenahamza54321@gmail.com>
2026-09-07 20:48:16 +0530

    commit

--- my-app/components/Header.tsx
+++ my-app/components/Header.tsx
@@ -28,7 +28,7 @@ export default function Header() {
 
                 </div>
                 <button className=" relative rounded-xl border border-white/10  p-3 text-gray-400 hover:bg-white/5 hover:text-white"    >
-
+<Bell size={18}/>
                 </button>
             </div>
         </header>
