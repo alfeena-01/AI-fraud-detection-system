a7b7246d464f4e425f881150e1158f15ede67e8f
alfeena-01 <alfeenahamza54321@gmail.com>
2026-09-17 20:30:33 +0530

    commit

--- my-app/components/RiskChart.tsx
+++ my-app/components/RiskChart.tsx
@@ -19,5 +19,13 @@ const data=[
     {day:"Sun", transactions: 5700, fraud: 129},
 ];
 
+export default function RiskChart() {
+    return (
+        <div className="h-[330px] w-full">
+            <ResponsiveContainer width="100%" height="100%">
+                <AreaChart data={data}>
+                    <defs>
+                        <linearGradient id="colorTransactions" x1="0" y1="0" x2="0" y2="1">
 
+        </div>
 
