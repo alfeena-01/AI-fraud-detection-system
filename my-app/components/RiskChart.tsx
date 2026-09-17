4aba0d78363d19aeaab22c76991dff922afa562e
alfeena-01 <alfeenahamza54321@gmail.com>
2026-09-17 20:32:36 +0530

    commit

--- my-app/components/RiskChart.tsx
+++ my-app/components/RiskChart.tsx
@@ -28,6 +28,8 @@ export default function RiskChart() {
                         <linearGradient id="transactionGradient" x1="0" y1="0"  x2="0" y2="1">
                             <stop offset="0%" stopOpacity={0.25}/>
                             <stop offset="100%" stopOpacity={0}/>
+                        </linearGradient>
+                        </defs>
 
         </div>
 
