656bd6026c1339591f6c367068eeaff3add2290e
alfeena-01 <alfeenahamza54321@gmail.com>
2026-09-17 20:38:43 +0530

    commit

--- my-app/components/RiskChart.tsx
+++ my-app/components/RiskChart.tsx
@@ -45,6 +45,8 @@ export default function RiskChart() {
                     <Tooltip contentStyle={{ backgroundColor: "#11161f", border: "1px solid rgba(255,255,255,0.1)", borderRadius: "12px" ,color: "#fff"}} />
                     <Area type="monotone" dataKey="transactions" stroke="#ffffff" strokeWidth={2} fill="url(#transactionGradient)" />
                     <Area type="monotone" dataKey="fraud" stroke="#777" strokeWidth={2} fillOpacity={2} fill="transparent" />
+                </AreaChart>
+            </ResponsiveContainer>
 
         </div>
 
