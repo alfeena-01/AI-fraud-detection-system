b9697c181be8ed2983148de712b7acb2b1991ed1
alfeena-01 <alfeenahamza54321@gmail.com>
2026-09-17 20:31:23 +0530

    commit

--- my-app/components/RiskChart.tsx
+++ my-app/components/RiskChart.tsx
@@ -25,7 +25,7 @@ export default function RiskChart() {
             <ResponsiveContainer width="100%" height="100%">
                 <AreaChart data={data}>
                     <defs>
-                        <linearGradient id="colorTransactions" x1="0" y1="0" x2="0" y2="1">
+                        <linearGradient id="transactionGradient" x1="0" y1="0"  x2="0" y2="1">
 
         </div>
 
