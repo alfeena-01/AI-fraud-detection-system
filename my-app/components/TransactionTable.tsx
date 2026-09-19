679b2381e835e385497f677d1ab03108e5bc3546
alfeena-01 <alfeenahamza54321@gmail.com>
2026-09-19 19:57:57 +0530

    commit

---
+++ my-app/components/TransactionTable.tsx
@@ -0,0 +1,10 @@
+const transactions = [
+    {
+        id: "TXN-92831",
+        user: "U-1028",
+        amount: "₹85,000",
+        location: "Germany",
+        risk: 94,
+        status: "Blocked",
+    },
+    
