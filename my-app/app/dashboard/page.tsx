e6d597980f5c6f146e7cf272c1867569c389bb98
alfeena-01 <alfeenahamza54321@gmail.com>
2026-09-22 18:50:40 +0530

    commit

--- my-app/app/dashboard/page.tsx
+++ my-app/app/dashboard/page.tsx
@@ -12,3 +12,9 @@ import Header from "@/components/Header";
 import StatCard from "@/components/StatCard";
 import RiskChart from "@/components/RiskChart";
 import TransactionTable from "@/components/TransactionTable";
+
+export default function DashboardPage() {
+  return (
+    <div className="min-h-screen bg-[#080b10] text-white">
+      
+      <Sidebar />
