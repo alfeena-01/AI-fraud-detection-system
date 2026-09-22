72f7c506f5680d85f0999171736c6bb169fe55f3
alfeena-01 <alfeenahamza54321@gmail.com>
2026-09-22 18:46:51 +0530

    first commit

--- my-app/app/dashboard/page.tsx
+++ my-app/app/dashboard/page.tsx
@@ -6,3 +6,9 @@ import {
   CircleDollarSign,
   ShieldAlert,
 } from "lucide-react";
+
+import Sidebar from "@/components/Sidebar";
+import Header from "@/components/Header";
+import StatCard from "@/components/StatCard";
+import RiskChart from "@/components/RiskChart";
+import TransactionTable from "@/components/TransactionTable";
